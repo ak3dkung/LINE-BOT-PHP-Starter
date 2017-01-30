87514aa1a082928f75d6fb57bdd95c25cdeb2bfd
ak3dkung <apichoke@hotmail.com>
2017-01-30 23:36:29 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,4 @@
-s<?php
+<?php
 $access_token = 'omDVoUcZhQUaJ9yvbNc0Z4XMGVz6n6KPx0rAosG7DEPQMr9NPgZ67M1QYGDeYzU4lBux+IBkb5NxzUztcK4OW3y6/8177cuivSaPOnpbhbyhhQzCZeab0I97nZtre5t9n00sU+DjQALJSqufPDyfiAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
@@ -6,28 +6,30 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 
-$replyToken = $events['replyToken'];
-$messages="ทดสอบ";
+foreach ($events['events'] as $event) {
+	$replyToken = $event['replyToken'];	
+	$messages = "ทดสอบ";
+	
+	// Make a POST Request to Messaging API to reply to sender
+	$url = 'https://api.line.me/v2/bot/message/reply';
+	$data = [
+		'replyToken' => $replyToken,
+		'messages' => [$messages],
+	];
+	$post = json_encode($data);
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-// Make a POST Request to Messaging API to reply to sender
-$url = 'https://api.line.me/v2/bot/message/reply';
-$data = [
-	'replyToken' => $replyToken,
-	'messages' => [$messages],
-];
-$post = json_encode($data);
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-
-echo $result . "\r\n";
+	echo $result . "\r\n";
+}
 
 echo "OK";
 ?>
